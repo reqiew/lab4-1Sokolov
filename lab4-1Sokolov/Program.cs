74f4b8cb6b3b6498a28bc17ba2a1923b65a8931c
dobry <dobry@DESKTOP-FBD3PAQ>
2023-10-31 04:43:05 +0300

    Добавьте файлы проекта.

---
+++ lab4-1Sokolov/Program.cs
@@ -0,0 +1,24 @@
+﻿//вариант 16 уровень средний
+Console.WriteLine("Введите элементы масcива");
+int[] mas = new int[5];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.Write(mas[i] + " ");
+}
+int sum = 0;
+int a = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    if (mas[i] > 0)
+    {
+        sum += mas[i];
+        a++;
+    }
+    else if (mas[i] < 0) break;
+}
+Console.WriteLine("sum =" + sum);
+Console.WriteLine("кол-во элементов = " + a);
